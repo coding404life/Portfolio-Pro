2e0b5d1fed9d0e9b15f1f0466c886ae0fb00997d
kingwar66 <kingwar66@gmail.com>
2020-12-03 19:45:03 +0200

    add resturant project to the portfolio

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 let clientIndex = 1;
 
 slide(clientIndex);
---
+++ public/resturant/js/script.js
@@ -0,0 +1,15 @@
+// My main script file
+
+
+//change the date input to today date
+document.querySelector("#today").valueAsDate = new Date();
+
+let x = document.getElementById('home');
+
+function yahya(haha, haha2) {
+    if (haa < 1) {
+        console.log('easy')
+    } else if (haa2 < 1) {
+        console.log('hard')
+    }
+}
